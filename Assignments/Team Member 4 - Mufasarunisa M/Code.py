9a8cb8058f3e5698b9420bf45f521e7024fd3fa1
Asvitha <77911099+lov3tod3f3nd@users.noreply.github.com>
2022-10-11 22:14:14 +0530

    Delete Code.py

--- Assignments/Team Member 4 - Mufasarunisa M/Code.py
+++
@@ -1,9 +0,0 @@
-def detect(temperature,humidity):
-    if(humidity<30):
-        if(temperature>30):
-            print("Humidity : ",humidity,"Temperature : ",temperature,"ALARM TURNED ON !!")
-            
-for i in range(0,15):
-    temperature = random.randint(15,50)
-    humidity = random.randint(15,50)
-    detect(temperature,humidity)
