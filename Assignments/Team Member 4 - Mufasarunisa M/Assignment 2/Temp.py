b5fd4e0092b4c8bac9c2b40cb1a16bf6d25d9113
Asvitha <77911099+lov3tod3f3nd@users.noreply.github.com>
2022-10-17 11:50:20 +0530

    Update Temp.py

--- Assignments/Team Member 4 - Mufasarunisa M/Assignment 2/Temp.py
+++ Assignments/Team Member 4 - Mufasarunisa M/Assignment 2/Temp.py
@@ -1,9 +1,11 @@
+import random
 def detect(temperature,humidity):
     if(humidity<30):
-        if(temperature>30):
-            print("Humidity : ",humidity,"Temperature : ",temperature,"ALARM TURNED ON !!")
+        if(temperature>50):
+            print("Humidity : ",humidity,"Temperature : ",temperature,
+                  "HAZARDOUS!!! TURN ON ALARM")
             
 for i in range(0,15):
-    temperature = random.randint(15,50)
-    humidity = random.randint(15,50)
+    humidity = random.randint(15,55)
+    temperature = random.randint(40,80)
     detect(temperature,humidity)
